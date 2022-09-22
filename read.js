dc7a0994b1da20d5ee8efaec77095cf18999f893
murjo06 <markvlogs228@gmail.com>
2022-09-22 17:12:30 +0200

    Some stuff

--- read.js
+++ read.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
 
 let data = fs.readFileSync("./loginStyle.css", "utf8").replace(/\n/g, "");
-console.log(data);
+let text = "var style = document.createElement('style');\nstyle.innerHTML = '" + data + "';\ndocument.body.appendChild(style);";
+fs.writeFileSync("./console.js", text);
